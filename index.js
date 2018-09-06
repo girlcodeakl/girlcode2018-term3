9993ffc9f1dff5e253d27b629d7f24832cc0f7bb
Madison King <madisonking@Madisons-MacBook-Pro.local>
2018-09-06 19:24:27 +1200

    Defult Image

--- index.js
+++ index.js
@@ -24,8 +24,12 @@ app.get('/posts', sendPostsList);
 function saveNewPost(request, response) {
   console.log(request.body.message); //write it on the command prompt so we can see
   let post= {};
-  post.message = request.body.message;
   post.image = request.body.image;
+  if (post.image === "") {
+   post.image = "https://i.ytimg.com/vi/PezifzPrIvc/maxresdefault.jpg"
+ }
+  post.message = request.body.message;
+  console.log (post);
   posts.push(post); //save it in our list
   response.send("thanks for your message. Press back to add another");
 }
